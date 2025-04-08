6c7172c66023528d0ad470207dba62ce29b1fc6b
Benjamìn de Jesùs <mcdreck276@gmail.com>
2025-04-08 16:49:37 -0600

    modificacion de servert

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -7,20 +7,3 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender
   }
 ];
-
-
-// src/app/app.routes.server.ts
-import { Routes } from '@angular/router';
-import { HomeComponent } from './component/home/home.component';
-import { ScaleImgComponent } from './component/scale-img/scale-img.component';
-import { ConvertidorImgComponent } from './component/convertidor-img/convertidor-img.component';
-import { Error404Component } from './component/error-404/error-404.component';
-import { BackgroundRemoverComponent } from './component/background-remover/background-remover.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent }, // home
-  { path: 'scale', component: ScaleImgComponent },
-  { path: 'converter', component: ConvertidorImgComponent },
-  { path: 'remover', component: BackgroundRemoverComponent },
-  { path: '**', component: Error404Component } // Ruta wildcard para capturar rutas no definidas
-];
