c5e134fd471f4e3b4e1f28131ef72eef1a0ab273
Benjamìn de Jesùs <mcdreck276@gmail.com>
2025-04-09 20:21:39 -0600

    prueva de proxy.conf.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: 'pixmorpher.online',
-    allowedHosts: ['pixmorpher.online', 'localhost'],
+    allowedHosts: ['pixmorpher.online', 'localhost',''],
   }
 });
