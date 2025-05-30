aa65d4c3adb4ccb7c64d8aaf1d3b0de36b0ea4dd
Benjamìn de Jesùs <mcdreck276@gmail.com>
2025-05-29 18:29:28 -0600

    agrege las indicaciones

--- start.sh
+++ start.sh
@@ -4,12 +4,19 @@
 limpieza() {
   echo "Ejecutando limpieza..."
   # Aquí va tu lógica de limpieza
+  pkill -9 nginx
+  pkill -9 cloudflared
+  pkill -9 screen
+  pkill -f "ng serve"
 }
 
 # Definir la función inicializacion
 inicializacion() {
   echo "Ejecutando inicialización..."
   # Aquí va tu lógica de inicialización
+  nginx
+  nohup cloudflared tunnel --pidfile /home/drymnz_compu_one/e/cloudflared.pid --no-autoupdate run --token eyJhIjoiYWFkYjUwMTZhNDAyNWY3ZWQzMTI2NjhmNTYxNDQwMzEiLCJ0IjoiMmNjNDE1NDctYjdkNC00ODhlLWEwZTYtOGZkNDMzOTBhZmU3IiwicyI6Ik5HRXlNalU0WWpVdE16TXdaUzAwWlRKaExUaGtNalF0TW1ObE5tUXpZakV3TTJVeiJ9 > cloudflared.log 2>&1 &
+  screen ng serve --proxy-config proxy.conf.json --port 4000 --host 192.168.1.58 --disable-host-check &
 }
 
 # Bucle infinito que ejecuta las funciones cada 24 horas
